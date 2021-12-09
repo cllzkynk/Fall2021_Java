e0020c1cd1dbc0556ef79d2662e3b3f2c2d2ffe8
cllzkynk@gmail.com <cllzkynk@gmail.com>
2021-12-09 23:30:43 +0200

    09/12/2021

---
+++ src/day37_Inheritance/Corolla.java
@@ -0,0 +1,27 @@
+package day37_Inheritance;
+
+public class Corolla {
+
+	String renk;
+	int fiyat;
+	boolean elektrikliMi;
+	int tekerSayisi=4;
+	String uretimYeri="Turkiye"; 
+	
+	public static void main(String[] args) {
+		
+		
+		Corolla arb1=new Corolla();
+		
+		// Java da obje olusturdugumuz her class ismi ayni zamanda bir data turu'dur
+		// Bu ornegimiz icin Corolla hem class adi, hem de olusturdugumuz arb1 objesinin data type'idir.
+		String str=new String("Ali Can");
+		
+		
+		
+
+		
+
+	}
+
+}
