c0cddddd3a0536c9da17520d61ba2ab009211e5c
cllzkynk@gmail.com <cllzkynk@gmail.com>
2021-12-18 17:30:04 +0200

    18/12/2021

--- src/deneme/Lion.java
+++ src/deneme/Lion.java
@@ -5,4 +5,17 @@ public class Lion extends BigCat {
 	public void roar () {
 		System.out.println("The leon lets out a loud ROAR! ");
 	}
+	public static void main(String[] args) {
+		 double a=10.2;
+		 float b=(float) 10.2;
+	System.out.println(a==b? "Esit" : "Degillll");
+	
+	int x=5;
+	short y=5;
+	System.out.println(x==y ? "Tamlarda calisti" : " Tamda da calismadi");
+	
+	
+	
+	
+	}
 }
