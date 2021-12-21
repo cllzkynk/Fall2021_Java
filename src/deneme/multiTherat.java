b8d3609282609d7090e6d93fd0bbe235af6474e5
cllzkynk@gmail.com <cllzkynk@gmail.com>
2021-12-21 20:55:30 +0200

    21/12/2021

---
+++ src/deneme/multiTherat.java
@@ -0,0 +1,18 @@
+package deneme;
+
+public class multiTherat {
+
+	static int a=100;
+	public static void main(String[] args) {
+		for (int i = 0; i <10; i++) {
+			a++;
+			System.out.println(a); 
+		}
+		for (int i = 0; i <10; i++) {
+			a--;
+			System.out.println(a);
+		}
+		 
+	}
+
+}
